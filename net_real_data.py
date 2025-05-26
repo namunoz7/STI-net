9426f8ae023a3c2675e29a4e7e8ae42da6b7f4b8
namunoz7 <namunoz7@uc.cl>
2025-05-26 12:36:56 -0400

    Update net_real_data.py
    Evaluate the trainig of only using the directions obtained from the first 6 orientations of the acquired Graz dataset

--- net_real_data.py
+++ net_real_data.py
@@ -11,11 +11,10 @@ Currently, the trained net is the finished net.
 import torch
 import os
 import numpy as np
-import sys
 import nibabel as nib
 import scipy
 import torch.nn.functional
-from Models.Pytorch.STIResNet_copy import STIResNetSplit
+from Models.Pytorch.STIResNetSplit_2 import STIResNetSplit
 from mms_msa_real_data import eig_decomposition
 from mms_msa_real_data import get_angle_images
 from time import time
@@ -31,6 +30,7 @@ if IS_COSMOS:
     RESULTS_FOLDER = os.path.join(SIMULATED_FOLDER, 'STI_net_angles')
 else:
     IMG_ROOT = '../Phantom_real_data/'
+    # IMG_ROOT = '../../Imagenes/Phantom_real_data/'
     is_flipped = False
     if is_flipped:
         MASK_FILE = os.path.join(IMG_ROOT, 'Masks/flipped_mask.nii.gz')
@@ -40,16 +40,16 @@ else:
         PHI_FILE = os.path.join(IMG_ROOT, 'Susceptibility_data/bulk_phase2.nii.gz')
     SIMULATED_FOLDER = os.path.join(IMG_ROOT, 'Susceptibility_data/')
     # RESULTS_FOLDER = os.path.join(SIMULATED_FOLDER, 'Real_data/STI_net_angles')
-    RESULTS_FOLDER = os.path.join(SIMULATED_FOLDER, 'Real_data/STI_partial_3')
+    RESULTS_FOLDER = os.path.join(SIMULATED_FOLDER, 'Real_data/STI_shuffled_May26-12:15')
     ATLAS_NAME = os.path.join(IMG_ROOT, 'Masks', 'atlas_mask.nii.gz')
     DTI_PEV_FILE = os.path.join(IMG_ROOT, 'Diffusion_data/DTI_reg_files/V1_filtered.nii.gz')
     DIRECTION_FIELD_FILE = os.path.join(SIMULATED_FOLDER, 'direction_field.mat')
 
 # Parameters
-directions_used = [0, 5, 4, 3, 2, 1]
+directions_used = [0, 5, 4, 1, 2, 3]
 CHECKPOINT_ROOT = os.path.join('checkpoints', 'Pytorch')
 # CHECKPOINT_FOLDER = 'STI_angles4'
-CHECKPOINT_FOLDER = 'STI_angles_shuffled3'
+CHECKPOINT_FOLDER = 'STInet_res-unet2'
 CHECKPOINT_NAME = 'net_model.pt'
 CHECKPOINT_PATH = os.path.join(CHECKPOINT_ROOT, CHECKPOINT_FOLDER, CHECKPOINT_NAME)
 ALPHA = 1
@@ -67,7 +67,6 @@ DEVICE = torch.device('cpu')
 GAMMA = 42.58
 B0 = 3  # T
 phase_scale = 2*np.pi*GAMMA*B0
-eps = sys.float_info.epsilon
 
 
 def read_img(filename):
@@ -91,42 +90,8 @@ def load_dir_field(dir_field_file, directions):
     """
     dir_field = scipy.io.loadmat(dir_field_file)
     dir_field = dir_field['direction_field']
-    dir_field = torch.tensor(dir_field[directions, :], dtype=torch.float32).reshape(18)
+    dir_field = torch.tensor(dir_field[directions, :], dtype=torch.float32).reshape(18).unsqueeze(0)
     return dir_field
-
-
-def fft_phase(inputs):
-    """
-    Gets the fourier transform of the input geometric figure images.
-    :param inputs: Input image
-    :return:
-    """
-    fft_input = torch.fft.fftn(input=inputs, dim=(0, 1, 2))
-    return fft_input
-
-
-def inv_fft_phase(fft_input):
-    """
-    Gets the inverse Fourier Transform
-    :param fft_input:
-    :return:
-    """
-    inputs = torch.fft.ifftn(fft_input, dim=(0, 1, 2))
-    return inputs
-
-
-def shift_fft(x, dims, mat_size):
-    """
-    Shift zero-frequency component to center of spectrum
-    :param mat_size:
-    :param x: Input image
-    :param dims: Dimensions to roll
-    :return:
-    """
-    x = x.roll((mat_size[0] // 2,
-                mat_size[1] // 2,
-                mat_size[2] // 2), dims)
-    return x
 
 
 def load_model(checkpoints_path):
@@ -176,30 +141,35 @@ def check_root(root_file):
 
 def main():
     print('Load actual image')
-    phi, nii = read_img(PHI_FILE)
-    mask, _, = read_img(MASK_FILE)
-    white_matter, _ = read_img(ATLAS_NAME)
-    pev_dti, _ = read_img(DTI_PEV_FILE)
-    white_matter = white_matter == 3
-    alpha_rc1 = ALPHA
+    # phi, nii = read_img(PHI_FILE)
+    phi, nii = read_img(os.path.join(RESULTS_FOLDER, 'phi_2.nii.gz'))
+    phi = torch.nn.functional.pad(phi, (0, 0, 48, 48), mode='constant', value=0).unsqueeze(0)
+    # alpha_rc1 = ALPHA
     direction_field = load_dir_field(DIRECTION_FIELD_FILE, directions_used)
-    if not IS_COSMOS:
-        phi = torch.nn.functional.pad(phi, (0, 0, 48, 48), mode='constant', value=0)
-        alpha_rc1 = 1
+    # if not IS_COSMOS:
+    #     phi = torch.nn.functional.pad(phi, (0, 0, 48, 48), mode='constant', value=0)
+    #     alpha_rc1 = 1
     check_root(SIMULATED_FOLDER)
     check_root(RESULTS_FOLDER)
     # phi = alpha_rc1 * phi[:, :, :, [0, 8, 1, 4, 5, 3]]
-    phi = alpha_rc1 * phi[:, :, :, :6]
-    save_img(nii, torch.squeeze(phi)[:, :, 48:-48, :], os.path.join(RESULTS_FOLDER, 'phi_2.nii.gz'))
+    # phi = alpha_rc1 * phi[:, :, :, directions_used]
+    # save_img(nii, torch.squeeze(phi)[:, :, 48:-48, :], os.path.join(RESULTS_FOLDER, 'phi_2.nii.gz'))
 
     print('Loading STInet model')
     sti_model = load_model(CHECKPOINT_PATH)
 
     print('Reconstructing sti image')
     t = time()
-    chi_model = sti_model(phi.unsqueeze(0), direction_field)
+    chi_model = sti_model(phi)
+    print(f"Chi model shape: {chi_model.shape}")
     t2 = time() - t
     print('Elapsed time: %.2f seconds' % t2)
+    del phi
+
+    mask, _, = read_img(MASK_FILE)
+    white_matter, _ = read_img(ATLAS_NAME)
+    pev_dti, _ = read_img(DTI_PEV_FILE)
+    white_matter = white_matter == 3
 
     if not IS_COSMOS:
         chi_model = torch.squeeze(chi_model)[:, :, 48:-48, :]
